9c429c36142b76b827b382c9842c8ba3d2988152
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2022-09-12 14:51:42 +0200

    little change

--- libft/libft/ft_strjoin.c
+++ libft/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 10:27:58 by halvarez          #+#    #+#             */
-/*   Updated: 2022/05/07 17:59:00 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/09/12 14:48:29 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	int		len;
 
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	len = i + j;
+	i = -1;
+	j = -1;
+	len = ft_strlen(s1) + ft_strlen(s2);
 	dest = malloc((len + 1) * sizeof(char));
 	if (!dest)
 		return ((void *)0);
-	i = -1;
-	j = -1;
-	while (s1[++i])
+	while (s1 && s1[++i])
 		dest[i] = s1[i];
-	while (s2[++j])
+	while (s2 && s2[++j])
 		dest[i + j] = s2[j];
 	while (i + j < len + 1)
 		dest[i + j++] = '\0';
